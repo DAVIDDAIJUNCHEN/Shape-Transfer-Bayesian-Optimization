e04858d3648a5ea8342a55c7749ad3184849680a
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-09-21 21:46:51 -0400

    add exp_mu and RKHS norm betwen mu1 m2

--- simfun.py
+++ simfun.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def branin(input=[3., 4.]):
     "Branin function: "
@@ -22,3 +23,40 @@ def mod_branin(input=[3., 4.]):
 
     return branin(input) + 20*x1 - 30*x2
 
+def exp_mu(input, mu, theta=1):
+    """
+    Exponential function on ||input - mu||^2,
+    mu and x are lists with same length
+    """
+    assert(len(input) == len(mu))
+    square_mu_input = [(xi - mu_i)**2 for xi, mu_i in zip(input, mu)]
+    norm2 = sum(square_mu_input)
+
+    exp_mu = np.exp(-0.5 * norm2 / theta**2)
+
+    return exp_mu
+
+def diff_mu1_mu2(mu1, mu2, theta=1):
+    "The RKHS norm of exp_mu(,mu1) - exp_mu(,mu2)"
+    norm_rkhs = np.sqrt(2 - 2 * exp_mu(mu1, mu2, theta))
+
+    return norm_rkhs
+
+
+if __name__ == "__main__":
+    x_draw = np.linspace(0, 7, 100)
+
+    y_theta_1 = [diff_mu1_mu2([0], [ele], theta=1) for ele in x_draw]
+    y_theta_1_2 = [diff_mu1_mu2([0], [ele], theta=0.5) for ele in x_draw]
+    y_theta_3 = [diff_mu1_mu2([0], [ele], theta=3) for ele in x_draw]
+    
+    fig, ax = plt.subplots(1, 1)
+    ax.set_title("RHKS Norm of mu1-mu2")
+    ax.plot(x_draw, y_theta_1_2, label="theta=0.5")
+    ax.plot(x_draw, y_theta_1, label="theta=1")
+    ax.plot(x_draw, y_theta_3, label="theta=3")
+
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig("./rhks_norm_mu1_mu2")
+    
