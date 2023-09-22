aa1899c89672e06e4c68aa2bdc09382801ae9524
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-09-21 21:47:35 -0400

    make exp data very similar in exp1 and 2

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     print(EI.X)
     print(EI.Y)
 
-    kessi = 1
+    kessi = 0
     x1 = [12]
     print("EI({:.2f}) = {:.2f}".format(x1[0], EI.aux_func_ei(x1, kessi)))
     x2 = [10.4]
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     start_points = [[12], [1], [13], [11]]
     next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=10)
     EI.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
+    print("Next Point: ")
 
     # STBO on Experiments 1 & 2 
     STBO = ShapeTransferBO()
@@ -34,15 +35,15 @@ if __name__ == "__main__":
 
     print(STBO.X)
     print(STBO.Y)
+    
+    x1 = [24]
+    print("STBO EI({:.2f}) = {:.2f}".format(x1[0], STBO.aux_func_ei(x1, kessi)))
+    x2 = [17]
+    print("STBO EI({:.2f}) = {:.2f}".format(x2[0], STBO.aux_func_ei(x2, kessi)))
 
-    print(STBO.zeroGP1.X)
-    print(STBO.zeroGP1.Y)
-
-    print(STBO.diffGP.X)
-    print(STBO.diffGP.Y)
 
     start_points = [[0], [13], [15], [20]]
-    next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=60)    
+    next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500)    
     STBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
     print(next_point)
 
@@ -61,14 +62,12 @@ if __name__ == "__main__":
     print(BCBO.X)
     print(BCBO.Y)
 
-    print(BCBO.zeroGP1.X)
-    print(BCBO.zeroGP1.Y)
-
-    print(BCBO.diffGP.X)
-    print(BCBO.diffGP.Y)
+    x1 = [24]
+    print("BCBO EI({:.2f}) = {:.2f}".format(x1[0], BCBO.aux_func_ei(x1, kessi)))
+    x2 = [17]
+    print("BCBO EI({:.2f}) = {:.2f}".format(x2[0], BCBO.aux_func_ei(x2, kessi)))
 
     start_points = [[0], [13], [15], [20]]
-    next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=60)    
+    next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500)    
     BCBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
     print(next_point)
-    
