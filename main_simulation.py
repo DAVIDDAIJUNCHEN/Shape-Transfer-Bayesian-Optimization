9c06b5d9ee042dd5cfef08995fe438232c59c3aa
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-09-24 21:34:09 -0400

    change EXP simulation output file name

--- main_simulation.py
+++ main_simulation.py
@@ -3,14 +3,13 @@
 import argparse, os, random
 import numpy as np
 
-from gp import ZeroGProcess
 from optimization import UpperConfidenceBound
 from optimization import ExpectedImprovement
 from optimization import BiasCorrectedBO
 from optimization import ShapeTransferBO
 
 from simfun import exp_mu
- 
+
 
 def arg_parser():
     "parse the argument"
@@ -50,9 +49,9 @@ def get_best_point(file, response_col=0):
 
     return best_point
 
-def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, kessi_1=0.0, file_1="f1.tsv",
+def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, kessi_1=0.0, file_1_gp="f1_gp.tsv",
              rand_file_1="rf1.tsv", num_start_opt2=5, lr2=0.5, num_steps_opt2=500, kessi_2=0.0, 
-             file_2_gp="f2_gp.tsv", file_2_stbo="f2_stbo.tsv", start_from_exp1=True):
+             file_2_gp="f2_gp.tsv", file_2_stbo="f2_stbo.tsv", file_2_bcbo="f2_bcbo.tsv", start_from_exp1=True):
     "main function of exponential target function type"
     theta = parser.theta
     mu1 = parser.mu1
@@ -68,10 +67,10 @@ def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, 
     low = -5
     high = 5
 
-    # Step 1: skip if start_from_exp1 is False
+    # Step 1: experiment 1 (skip if start_from_exp1 is False)
     if start_from_exp1:
         # write header & init_point to file: file_1 (ZeroGP) & rand_file_1 (random search)
-        with open(file_1, "w", encoding="utf-8") as f1:
+        with open(file_1_gp, "w", encoding="utf-8") as f1:
             header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
             f1.writelines(header_line)
 
@@ -82,30 +81,30 @@ def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, 
         # random initialization in exp 1
         init_point_1 = np.random.uniform(low, high, size = dim)
         init_res_1 = exp_mu(init_point_1, mu1, theta)
-        write_exp_result(file_1, init_res_1, init_point_1)
+        write_exp_result(file_1_gp, init_res_1, init_point_1)
         write_exp_result(rand_file_1, init_res_1, init_point_1)
 
         # run num_exp1 times on EXP 1 by random search (rand_file_1) & ZeroGP (file_1)
         if num_exp1 > 1:
             for round_k in range(num_exp1-1):
-                # randomly pick next point
+                # uniformly randomly pick next point
                 next_point_rand = np.random.uniform(low, high, size = dim)
                 next_response_rand = exp_mu(next_point_rand, mu1, theta)
                 write_exp_result(rand_file_1, next_response_rand, next_point_rand)
 
                 # ZeroGProcess model with EI 
                 EI = ExpectedImprovement()
-                EI.get_data_from_file(file_1)
+                EI.get_data_from_file(file_1_gp)
 
                 start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt1)]
 
                 next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=lr1, num_step=num_steps_opt1, kessi=kessi_1)
                 next_response = exp_mu(next_point, mu1, theta)
-                write_exp_result(file_1,  next_response, next_point)
+                write_exp_result(file_1_gp,  next_response, next_point)
 
     # Step 2: Optimization on Experiemnt 2 
     # get best point from exp1 file and get value of exp2 on best point
-    best_point_exp1 = get_best_point(file_1)
+    best_point_exp1 = get_best_point(file_1_gp)
     res2_point_exp1 = exp_mu(best_point_exp1, mu2, theta)
 
     # write header and init point for GP method
@@ -117,43 +116,57 @@ def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, 
         header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
         f2.writelines(header_line)
 
+    with open(file_2_bcbo, "w", encoding="utf-8") as f2:
+        header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
+        f2.writelines(header_line)    
+
     write_exp_result(file_2_gp, res2_point_exp1, best_point_exp1)
     write_exp_result(file_2_stbo, res2_point_exp1, best_point_exp1)
+    write_exp_result(file_2_bcbo, res2_point_exp1, best_point_exp1)
 
-    if num_exp2 <=1:
-        return 0
+    if num_exp2 > 1:
+        for round_k in range(num_exp2-1):
+            # Method 1: ZeroGProcess model based on EI
+            EI = ExpectedImprovement()
+            EI.get_data_from_file(file_2_gp)
 
-    for round_k in range(num_exp2-1):
-        # Method 1: ZeroGProcess model based on EI
-        EI = ExpectedImprovement()
-        EI.get_data_from_file(file_2_gp)
+            start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
+            next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=lr2, 
+                                                                   num_step=num_steps_opt2, kessi=kessi_2)
+            next_response = exp_mu(next_point, mu2, theta)
+            write_exp_result(file_2_gp, next_response, next_point)
 
-        start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
-        next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=lr2, num_step=num_steps_opt2, kessi=kessi_2)
-        next_response = exp_mu(next_point, mu2, theta)
-        write_exp_result(file_2_gp, next_response, next_point)
+            # Method 2: STBO mothod based on EI from our paper
+            STBO = ShapeTransferBO()
+            STBO.get_data_from_file(file_2_stbo)
+            STBO.build_task1_gp(file_1_gp)
+            STBO.build_diff_gp()
 
-        # Method 2: STBO mothod based on EI
-        STBO = ShapeTransferBO()
-        STBO.get_data_from_file(file_2_stbo)
-        STBO.build_task1_gp(file_1)
-        STBO.build_diff_gp()
+            start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
+            next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=lr2, 
+                                                                     num_step=num_steps_opt2, kessi=kessi_2)                 
+            next_response = exp_mu(next_point, mu2, theta)
+            write_exp_result(file_2_stbo, next_response, next_point)        
 
-        start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
-        next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=lr2, num_step=num_steps_opt2, kessi=kessi_2)                 
-        next_response = exp_mu(next_point, mu2, theta)
-        write_exp_result(file_2_stbo, next_response, next_point)        
+            # Method 3: BCBO method based on EI from some other paper
+            BCBO = BiasCorrectedBO()
+            BCBO.get_data_from_file(file_2_bcbo)
+            BCBO.build_task1_gp(file_1_gp)
+            BCBO.build_diff_gp()
 
-        # Method 3: BCBO method based on EI
-         
+            start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
+            next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=lr2, 
+                                                                     num_step=num_steps_opt2, kessi=kessi_2)                 
+            next_response = exp_mu(next_point, mu2, theta)
+            write_exp_result(file_2_bcbo, next_response, next_point)        
 
     return 0
 
 
 def main_br(num_exp1, num_exp2):
     "main function of Brainn target function type"
-    pass
-
+    
+    return 0
 
 
 if __name__ == "__main__":
@@ -164,13 +177,21 @@ if __name__ == "__main__":
     T2 = int(parser.T2)
 
     if fun_type == "EXP":
-        f1 = "data/simulation_points_task1.tsv"
-        rf1="data/simulation_points_task1_rand.tsv"
-        f2_gp = "data/simulation_points_task2_gp.tsv"
-        f2_stbo = "data/simulation_points_task2_stbo.tsv"
+        f1_gp = "data/simExp_points_task1_gp.tsv"
+        f1_rnd = "data/simExp_points_task1_rand.tsv"
+        f2_gp = "data/simExp_points_task2_gp.tsv"
+        f2_stbo = "data/simExp_points_task2_stbo.tsv"
+        f2_bcbo = "data/simExp_points_task2_bcbo.tsv"
 
-        main_exp(T1, T2, file_1=f1, rand_file_1=rf1, file_2_gp=f2_gp, file_2_stbo=f2_stbo, start_from_exp1=True)
-
+        main_exp(T1, T2, file_1_gp=f1_gp, rand_file_1=f1_rnd, file_2_gp=f2_gp, 
+                 file_2_stbo=f2_stbo, file_2_bcbo=f2_bcbo, start_from_exp1=True)
+        
     elif fun_type == "BR":
-        main_br(T1, T2)
-
+        f1_gp = "data/simBr_points_task1_gp.tsv"
+        rf1 = "data/simBr_points_task1_rand.tsv"
+        f2_gp = "data/simBr_points_task2_gp.tsv"
+        f2_stbo = "data/simBr_points_task2_stbo.tsv"
+        f2_bcbo = "data/simBr_points_task2_bcbo.tsv"  
+              
+        # main_br(T1, T2, file_1_gp=f1_gp, rand_file_1=rf1, file_2_gp=f2_gp, 
+        #          file_2_stbo=f2_stbo, file_2_bcbo=f2_bcbo, start_from_exp1=True)
--- run_simulation.sh
+++ run_simulation.sh
@@ -5,7 +5,7 @@ Thetas="1"
 mu_1="0.0_0.0"
 mu_2="1.0_1.0"
 
-T1=10
+T1=15
 T2=4
 
 num_rep=1
@@ -13,6 +13,7 @@ num_rep=1
 for i in $(seq 1 $num_rep); do
     for theta in $Thetas; do
         python ./main_simulation.py  --T1 $T1  --T2 $T2  --type EXP  --mu1 $mu_1  --mu2 $mu_2  --theta $theta 
+        
     done
 done 
 
