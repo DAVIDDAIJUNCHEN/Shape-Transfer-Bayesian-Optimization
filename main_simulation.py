8587a95ac018eb17cbf4b3d65e7aa710ecac8070
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-09-25 06:56:11 -0400

    add simulation for Branin and modify Br

--- main_simulation.py
+++ main_simulation.py
@@ -8,7 +8,7 @@ from optimization import ExpectedImprovement
 from optimization import BiasCorrectedBO
 from optimization import ShapeTransferBO
 
-from simfun import exp_mu
+from simfun import exp_mu, branin, mod_branin
 
 
 def arg_parser():
@@ -52,7 +52,19 @@ def get_best_point(file, response_col=0):
 def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, kessi_1=0.0, file_1_gp="f1_gp.tsv",
              rand_file_1="rf1.tsv", num_start_opt2=5, lr2=0.5, num_steps_opt2=500, kessi_2=0.0, 
              file_2_gp="f2_gp.tsv", file_2_stbo="f2_stbo.tsv", file_2_bcbo="f2_bcbo.tsv", start_from_exp1=True):
-    "main function of exponential target function type"
+    """
+    simulation main function of Brainn target function type:
+    num_exp[1 | 2]: number of experiments in task [1 | 2]
+    num_start_opt[1 | 2]: number of start points in optimizing AC function in task [1 | 2]
+    lr[1 | 2]: learning rate used in optimizing AC function in task [1 | 2]
+    num_steps_opt[1 | 2]: number of steps in optimizing AC function in task [1 | 2]
+    kessi_[1 | 2]: kessi value used in AC function in task [1 | 2]
+    file_[1 | 2]_gp: file of experiment points choosen by zeroGP in task [1 | 2]
+    rand_file_1: file of experiemnt points choosen by random search in task 1
+    file_2_stbo: file of experiment points choosen by our STBO in task 2
+    file_2_bcbo: file of experiment points choosen by BCBO (bias corrected bayesian optimization) method
+    start_from_exp1: True | False, consider False if skip experiment 1 
+    """
     theta = parser.theta
     mu1 = parser.mu1
     mu2 = parser.mu2
@@ -163,9 +175,120 @@ def main_exp(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, 
     return 0
 
 
-def main_br(num_exp1, num_exp2):
-    "main function of Brainn target function type"
-    
+def main_br(num_exp1, num_exp2, num_start_opt1=5, lr1=0.5, num_steps_opt1=500, kessi_1=0.0, file_1_gp="f1_gp.tsv",
+            rand_file_1="rf1.tsv", num_start_opt2=5, lr2=0.5, num_steps_opt2=500, kessi_2=0.0, 
+            file_2_gp="f2_gp.tsv", file_2_stbo="f2_stbo.tsv", file_2_bcbo="f2_bcbo.tsv", start_from_exp1=True):
+    """
+    simulation main function of Brainn target function type:
+    num_exp[1 | 2]: number of experiments in task [1 | 2]
+    num_start_opt[1 | 2]: number of start points in optimizing AC function in task [1 | 2]
+    lr[1 | 2]: learning rate used in optimizing AC function in task [1 | 2]
+    num_steps_opt[1 | 2]: number of steps in optimizing AC function in task [1 | 2]
+    kessi_[1 | 2]: kessi value used in AC function in task [1 | 2]
+    file_[1 | 2]_gp: file of experiment points choosen by zeroGP in task [1 | 2]
+    rand_file_1: file of experiemnt points choosen by random search in task 1
+    file_2_stbo: file of experiment points choosen by our STBO in task 2
+    file_2_bcbo: file of experiment points choosen by BCBO (bias corrected bayesian optimization) method
+    start_from_exp1: True | False, consider False if skip experiment 1 
+    """
+
+    dim = 2 
+    low = -5
+    high = 5
+
+    # Step 1: experiment 1, branin (skip if start_from_exp1 is False)
+    if start_from_exp1:
+        # write header & init_point to file: file_1 (ZeroGP) & rand_file_1 (random search)
+        with open(file_1_gp, "w", encoding="utf-8") as f1:
+            header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
+            f1.writelines(header_line)
+
+        with open(rand_file_1, "w", encoding="utf-8") as f1:
+            header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
+            f1.writelines(header_line)
+
+        # random initialization in exp 1
+        init_point_1 = np.random.uniform(low, high, size = dim)
+        init_res_1 = branin(init_point_1)
+        write_exp_result(file_1_gp, init_res_1, init_point_1)
+        write_exp_result(rand_file_1, init_res_1, init_point_1)
+
+        # run num_exp1 times on EXP 1 by random search (rand_file_1) & ZeroGP (file_1)
+        if num_exp1 > 1:
+            for round_k in range(num_exp1 - 1):
+                # uniform randomly pick next point
+                next_point_rand = np.random.uniform(low, high, size = dim)
+                next_response_rand = branin(next_point_rand)
+                write_exp_result(rand_file_1, next_response_rand, next_point_rand)
+
+                # ZeroGProcess model with EI
+                EI = ExpectedImprovement()
+                EI.get_data_from_file(file_1_gp)
+
+                start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt1)]
+
+                next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=lr1, num_step=num_steps_opt1, kessi=kessi_1)
+                next_response = branin(next_point)
+                write_exp_result(file_1_gp,  next_response, next_point)
+
+    # Step 2: Optimization on Experiemnt 2 (mod_branin)
+    # get best point from exp1 file and get value of exp2 on best point
+    best_point_exp1 = get_best_point(file_1_gp)
+    res2_point_exp1 = mod_branin(best_point_exp1)
+
+    # write header and init point for GP method
+    with open(file_2_gp, "w", encoding="utf-8") as f2:
+        header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
+        f2.writelines(header_line)
+
+    with open(file_2_stbo, "w", encoding="utf-8") as f2:
+        header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
+        f2.writelines(header_line)
+
+    with open(file_2_bcbo, "w", encoding="utf-8") as f2:
+        header_line = "response" + ''.join(["#dim"+str(i+1) for i in range(dim)]) + '\n'
+        f2.writelines(header_line)    
+
+    write_exp_result(file_2_gp, res2_point_exp1, best_point_exp1)
+    write_exp_result(file_2_stbo, res2_point_exp1, best_point_exp1)
+    write_exp_result(file_2_bcbo, res2_point_exp1, best_point_exp1)
+
+    if num_exp2 > 1:
+        for round_k in range(num_exp2-1):
+            # Method 1: ZeroGProcess model based on EI
+            EI = ExpectedImprovement()
+            EI.get_data_from_file(file_2_gp)
+
+            start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
+            next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=lr2, 
+                                                                   num_step=num_steps_opt2, kessi=kessi_2)
+            next_response = mod_branin(next_point)
+            write_exp_result(file_2_gp, next_response, next_point)
+
+            # Method 2: STBO mothod based on EI from our paper
+            STBO = ShapeTransferBO()
+            STBO.get_data_from_file(file_2_stbo)
+            STBO.build_task1_gp(file_1_gp)
+            STBO.build_diff_gp()
+
+            start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
+            next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=lr2, 
+                                                                     num_step=num_steps_opt2, kessi=kessi_2)                 
+            next_response = mod_branin(next_point)
+            write_exp_result(file_2_stbo, next_response, next_point)        
+
+            # Method 3: BCBO method based on EI from some other paper
+            BCBO = BiasCorrectedBO()
+            BCBO.get_data_from_file(file_2_bcbo)
+            BCBO.build_task1_gp(file_1_gp)
+            BCBO.build_diff_gp()
+
+            start_points = [np.random.uniform(low, high, size=dim).tolist() for i in range(num_start_opt2)]
+            next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=lr2, 
+                                                                     num_step=num_steps_opt2, kessi=kessi_2)                 
+            next_response = mod_branin(next_point)
+            write_exp_result(file_2_bcbo, next_response, next_point)
+
     return 0
 
 
@@ -178,20 +301,21 @@ if __name__ == "__main__":
 
     if fun_type == "EXP":
         f1_gp = "data/simExp_points_task1_gp.tsv"
-        f1_rnd = "data/simExp_points_task1_rand.tsv"
+        f1_rand = "data/simExp_points_task1_rand.tsv"
         f2_gp = "data/simExp_points_task2_gp.tsv"
         f2_stbo = "data/simExp_points_task2_stbo.tsv"
         f2_bcbo = "data/simExp_points_task2_bcbo.tsv"
 
-        main_exp(T1, T2, file_1_gp=f1_gp, rand_file_1=f1_rnd, file_2_gp=f2_gp, 
+        main_exp(T1, T2, file_1_gp=f1_gp, rand_file_1=f1_rand, file_2_gp=f2_gp, 
                  file_2_stbo=f2_stbo, file_2_bcbo=f2_bcbo, start_from_exp1=True)
         
     elif fun_type == "BR":
         f1_gp = "data/simBr_points_task1_gp.tsv"
-        rf1 = "data/simBr_points_task1_rand.tsv"
+        f1_rand = "data/simBr_points_task1_rand.tsv"
         f2_gp = "data/simBr_points_task2_gp.tsv"
         f2_stbo = "data/simBr_points_task2_stbo.tsv"
         f2_bcbo = "data/simBr_points_task2_bcbo.tsv"  
               
-        # main_br(T1, T2, file_1_gp=f1_gp, rand_file_1=rf1, file_2_gp=f2_gp, 
-        #          file_2_stbo=f2_stbo, file_2_bcbo=f2_bcbo, start_from_exp1=True)
+        main_br(T1, T2, file_1_gp=f1_gp, rand_file_1=f1_rand, file_2_gp=f2_gp, 
+                file_2_stbo=f2_stbo, file_2_bcbo=f2_bcbo, start_from_exp1=True)
+
--- run_simulation.sh
+++ run_simulation.sh
@@ -1,32 +1,46 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-# Part 1: Exponential target function
-Thetas="1"
-mu_1="0.0_0.0"
-mu_2="1.0_1.0"
+stage=$1   # 0: run both EXP and BR; 1: run EXP only; 2: run BR only
 
-T1=15
-T2=4
+# check arguments
+if [ -z $1 ]; then 
+    echo "Usage: bash run_simulation.sh <stage-number>" 
+    echo "<stage-number>: 0, run all simulation;"
+    echo "<stage-number>: 1, run EXP only;"
+    echo "<stage-number>: 2, run BR only." && exit 0
+fi
 
-num_rep=1
 
-for i in $(seq 1 $num_rep); do
-    for theta in $Thetas; do
-        python ./main_simulation.py  --T1 $T1  --T2 $T2  --type EXP  --mu1 $mu_1  --mu2 $mu_2  --theta $theta 
-        
+if [ $stage -eq 0 ] || [ $stage -eq 1 ]; then 
+    echo "Simulation 1: Exponential target function"
+    # Taks 1: mu_1, theta; Taks 2: mu_2, theta
+    Thetas="1"
+    mu_1="0.0_0.0"
+    mu_2="1.0_1.0"
+
+    T1=15
+    T2=4
+
+    num_rep=1
+
+    for i in $(seq 1 $num_rep); do
+        for theta in $Thetas; do
+            python ./main_simulation.py  --T1 $T1  --T2 $T2  --type EXP  --mu1 $mu_1  --mu2 $mu_2  --theta $theta 
+
+        done
+    done 
+fi
+
+if [ $stage -eq 0 ] || [ $stage -eq 2 ]; then
+    echo "Simulation 2: Branni function (task1), Modified Branni function (task2)"
+    # Task 1: Branni; Task 2: Modified Branni
+    T1=15
+    T2=4
+
+    num_rep=1
+
+    for i in $(seq 1 $num_rep); do 
+        python ./main_simulation.py --T1 $T1  --T2 $T2 --type BR 
+
     done
-done 
-
-
-
-# Part 2: Branni & Modified Branni function 
-T1=10
-T2=4
-
-num_rep=1
-
-for i in seq 1 $num_rep; do 
-    python ./main_simulation.py --T1 $T1  --T2 $T2 --type BR 
-
-done
-
+fi
