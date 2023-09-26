17778932981c45166dd8ce80f06ae8e6bc8bf835
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-09-26 09:02:32 -0400

    add analyze tool

--- analyze_results.py
+++ analyze_results.py
@@ -6,6 +6,6 @@ import numpy as np
 def arg_parser():
     "parse the arguments"
     argparser = argparse.ArgumentParser(description="Analyze results in a dir")
-    argparser.add_argument()
-    
+    argparser.add_argument("input_dir", description="input dir containing subdirs of experiments")
+    argparser.add_argument("")    
     
