40da83ed38288408e978aaab9e43f66acc9ff459
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-09-26 04:05:37 -0400

    add analyzing python tool

---
+++ analyze_results.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+import argparse, os, random
+import numpy as np
+
+
+def arg_parser():
+    "parse the arguments"
+    argparser = argparse.ArgumentParser(description="Analyze results in a dir")
+    argparser.add_argument()
+    
+    
