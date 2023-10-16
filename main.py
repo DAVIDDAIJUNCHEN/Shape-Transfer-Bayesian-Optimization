6610824db5f9ddab6af13ff6e1af3d08c453d956
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-10-15 23:10:07 -0400

    add real example part

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env /mnt/users/daijun_chen/tools/miniconda3.10/install/envs/python3_huggingface/bin/python3
 
 import os 
 import numpy as np
@@ -10,63 +10,85 @@ from optimization import BiasCorrectedBO
 
 
 if __name__ == "__main__":
-    # EI on Experiment 1
-    EI = ExpectedImprovement()
-    EI.get_data_from_file("data/experiment_points_task1.tsv")
-    print(EI.X)
-    print(EI.Y)
+    task = 1
 
-    kessi = 0
-    x1 = [12]
-    print("EI({:.2f}) = {:.2f}".format(x1[0], EI.aux_func_ei(x1, kessi)))
-    x2 = [10.4]
-    print("EI({:.2f}) = {:.2f}".format(x2[0], EI.aux_func_ei(x2, kessi)))
+    if task == 1:
+        # Part 1: EI on Experiment 1
+        # 1.1 ZeroGP EI initialization
+        EI_task1 = ExpectedImprovement()
+        EI_task1.get_data_from_file("data/experiment_points_task1_gp.tsv")
+        print(EI_task1.X)
+        print(EI_task1.Y)
 
-    start_points = [[12], [1], [13], [11]]
-    next_point, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=10)
-    EI.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
-    print("Next Point: ")
+        # 1.1 AC optimization 
+        # dim = EI.dim
+        # low_opt1 = 0, high_opt1 = 1000
+        # num_start_opt1 = 15
+        # start_points = [np.random.uniform(low_opt1, high_opt1, size=dim).tolist() for i in range(num_start_opt1)]
+        start_points = [[12], [1], [13], [11]]
+        kessi = 0
 
-    # STBO on Experiments 1 & 2 
-    STBO = ShapeTransferBO()
-    STBO.get_data_from_file("./data/experiment_points_task2.tsv")
-    STBO.build_task1_gp("./data/experiment_points_task1.tsv")
-    STBO.build_diff_gp()
+        next_point, next_point_aux = EI_task1.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=800)
+        # EI.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
+        print("GP next point in task1: ", next_point)
+    elif task == 2:
+        # Part 2: EI on Experiment 2
+        # 2.1 ZeroGP EI initialization
+        EI_task2 = ExpectedImprovement()
+        EI_task2.get_data_from_file("data/experiment_points_task2_gp.tsv")
+        print(EI_task2.X)
+        print(EI_task2.Y)
 
-    print(STBO.X)
-    print(STBO.Y)
-    
-    x1 = [24]
-    print("STBO EI({:.2f}) = {:.2f}".format(x1[0], STBO.aux_func_ei(x1, kessi)))
-    x2 = [17]
-    print("STBO EI({:.2f}) = {:.2f}".format(x2[0], STBO.aux_func_ei(x2, kessi)))
+        # 2.1 AC optimization 
+        # dim = EI.dim
+        # low_opt1 = 0, high_opt1 = 1000
+        # num_start_opt1 = 15
+        # start_points = [np.random.uniform(low_opt1, high_opt1, size=dim).tolist() for i in range(num_start_opt1)]
+        start_points = [[12], [1], [13], [11]]
+        kessi = 0
 
-    start_points = [[0], [13], [15], [20]]
-    next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500)    
-    STBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
-    print(next_point)
+        next_point, next_point_aux = EI_task2.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=800)
+        # EI.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
+        print("GP next point in task1: ", next_point)        
 
-    # BCBO on Experiment 1 & 2
-    BCBO = BiasCorrectedBO()
-    BCBO.get_data_from_file("data/experiment_points_task2.tsv")
+        # Part 2: STBO on Experiments 2
+        # 2.1 STBO EI initialization
+        STBO = ShapeTransferBO()
+        STBO.get_data_from_file("./data/experiment_points_task2_STBO.tsv")
+        STBO.build_task1_gp("./data/experiment_points_task1_gp.tsv")
+        STBO.build_diff_gp()
 
-    print("before bias correction")
-    print(BCBO.X)
-    print(BCBO.Y)
+        # 2.2 AC optimization
+        # dim = STBO.dim
+        # low_opt2 = 0, high_opt2 = 1000
+        # num_start_opt2 = 15
+        # start_points = [np.random.uniform(low_opt2, high_opt2, size=dim).tolist() for i in range(num_start_opt2)]
+        start_points = [[0], [13], [15], [20]]
+        kessi = 0
 
-    BCBO.build_task1_gp("./data/experiment_points_task1.tsv")
-    BCBO.build_diff_gp()
+        next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=800)    
+        # STBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
+        print("STBO next point in task2: ", next_point)
 
-    print("after bias correction & merge")
-    print(BCBO.X)
-    print(BCBO.Y)
+        # Part 3: BCBO on Experiment 2
+        # 3.1 BCBO EI initialization
+        BCBO = BiasCorrectedBO()
+        BCBO.get_data_from_file("data/experiment_points_task2_BCBO.tsv")
 
-    x1 = [24]
-    print("BCBO EI({:.2f}) = {:.2f}".format(x1[0], BCBO.aux_func_ei(x1, kessi)))
-    x2 = [17]
-    print("BCBO EI({:.2f}) = {:.2f}".format(x2[0], BCBO.aux_func_ei(x2, kessi)))
+        print("before bias correction")
+        print(BCBO.X)
+        print(BCBO.Y)
 
-    start_points = [[0], [13], [15], [20]]
-    next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500)    
-    BCBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
-    print(next_point)
+        BCBO.build_task1_gp("./data/experiment_points_task1_gp.tsv")
+        BCBO.build_diff_gp()
+
+        print("after bias correction & merge")
+        print(BCBO.X)
+        print(BCBO.Y)
+
+        # 3.2 AC optimization
+        kessi = 0
+
+        next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500)    
+        # BCBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
+        print("BCBO next point in task2: ", next_point)
