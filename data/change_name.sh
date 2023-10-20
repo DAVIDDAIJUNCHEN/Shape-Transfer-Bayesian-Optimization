93c86ed49390abbc27540b11989b6d833493ad6c
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-10-19 21:20:15 -0400

    remove out of date script

--- data/change_name.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash 
-
-for i in $(seq 1 20); do 
-	mv $i/simExp_points_task2_bcbo.tsv $i/simExp_points_task2_bcbo_from_gp.tsv
-	mv $i/simExp_points_task2_gp.tsv   $i/simExp_points_task2_gp_from_gp.tsv
-	mv $i/simExp_points_task2_stbo.tsv $i/simExp_points_task2_stbo_from_gp.tsv
-done 
-
