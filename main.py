64086b7ea583081293c3dd980f7631460fd5bb36
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-11-03 04:59:14 -0400

    add real experiments

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from optimization import BiasCorrectedBO
 
 if __name__ == "__main__":
     """" Run Real experiment """
-    task = 1
+    task = 2
 
     if task == 1:
         # Part 1: EI on Experiment 1
@@ -42,16 +42,21 @@ if __name__ == "__main__":
         print(EI_task2.Y)
 
         # 2.2 AC optimization 
-        # dim = EI.dim
-        # low_opt1 = 0, high_opt1 = 1000
-        # num_start_opt1 = 15
+        # dim = EI_task2.dim
+        # low_opt1 = 0; high_opt1 = 5000
+        # num_start_opt1 = 5000
         # start_points = [np.random.uniform(low_opt1, high_opt1, size=dim).tolist() for i in range(num_start_opt1)]
-        start_points = [[12], [1], [13], [11]]
+
         kessi = 0
 
-        next_point, next_point_aux = EI_task2.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=800, kessi=kessi)
+        EI_task1 = ExpectedImprovement()
+        EI_task1.get_data_from_file("data/experiment_points_task1_gp.tsv")
+        start_points = EI_task1.X
+
+        next_point, next_point_aux = EI_task2.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500, kessi=kessi)
+
         # EI.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
-        print("GP next point in task2: ", next_point)        
+        print("GP next point in task2: ", next_point, " AC: ", next_point_aux)        
 
         # Part 3: STBO on Experiments 2
         # 3.1 STBO EI initialization
@@ -60,12 +65,13 @@ if __name__ == "__main__":
         STBO.build_task1_gp("./data/experiment_points_task1_gp.tsv")
         STBO.build_diff_gp()
 
+        print([STBO.aux_func_ei(pt) for pt in start_points[0:2]])
         # 3.2 AC optimization (shared the same start points as in gp)
         kessi = 0
 
-        next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=800, kessi=kessi)    
+        next_point, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500, kessi=kessi)    
         # STBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
-        print("STBO next point in task2: ", next_point)
+        print("STBO next point in task2: ", next_point, " AC: ", next_point_aux)
 
         # Part 4: BCBO on Experiment 2
         # 4.1 BCBO EI initialization
@@ -88,7 +94,7 @@ if __name__ == "__main__":
 
         next_point, next_point_aux = BCBO.find_best_NextPoint_ei(start_points, learn_rate=0.5, num_step=500, kessi=kessi)    
         # BCBO.plot_ei(kessis=[0.0], num_points=300, highlight_point=[next_point, next_point_aux])
-        print("BCBO next point in task2: ", next_point)
+        print("BCBO next point in task2: ", next_point, " AC: ", next_point_aux)
     else:
         raise(ValueError)
 
