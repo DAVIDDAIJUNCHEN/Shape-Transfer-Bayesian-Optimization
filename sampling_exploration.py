526c6d6eeefd95eb596bd462bbe4c83bfdf3ca0f
DAVIDDAIJUNCHEN <daijun.chen@cerence.com>
2023-12-12 21:08:38 -0500

    update simulation results to explore sampling

--- sampling_exploration.py
+++ sampling_exploration.py
@@ -44,10 +44,10 @@ def generate_task1_samples(mu, theta, num, low, high, out_file):
 
 if __name__ == "__main__":
     # Part 1: Weak Prior 
-    mu=[7.5]; theta=1; num=30; low=-5; high=15
-    out_file_task1 = "./data/Sample_Exp/f1_mu_7.5_theta_1.tsv"
-    out_file_task2_stbo = "./data/Sample_Exp/f2_stbo_prior_mu_7.5_theta_1.tsv"
-    out_file_task2_gp = "./data/Sample_Exp/f2_gp_prior_mu_7.5_theta_1.tsv"
+    mu=[6.5]; theta=1; num=30; low=-5; high=15
+    out_file_task1 = "./data/Sample_Exp/f1_mu_6.5_theta_1.tsv"
+    out_file_task2_stbo = "./data/Sample_Exp/f2_stbo_prior_mu_6.5_theta_1.tsv"
+    out_file_task2_gp = "./data/Sample_Exp/f2_gp_prior_mu_6.5_theta_1.tsv"
 
     stage = 2
 
@@ -56,10 +56,16 @@ if __name__ == "__main__":
         best_point_task1 = get_best_point(out_file_task1)
         res2_point = triple_fun(best_point_task1)
 
+        with open(out_file_task2_stbo, "w", encoding="utf-8") as fin:
+            fin.writelines("response#dim1" + '\n')
+        
+        with open(out_file_task2_gp, "w", encoding="utf-8") as fin:
+            fin.writelines("response#dim1" + '\n')
+
         write_exp_result(out_file_task2_stbo, res2_point, best_point_task1)
         write_exp_result(out_file_task2_gp, res2_point, best_point_task1)
     elif stage == 2:
-        num_start = 25; lr = 0.5; num_step=20
+        num_start = 30; lr = 0.5; num_step=20
         start_points = [np.random.uniform(low, high, size=1).tolist() for i in range(num_start)]
 
         # draw picture by EI
@@ -78,12 +84,11 @@ if __name__ == "__main__":
         next_point_gp, next_point_aux = EI.find_best_NextPoint_ei(start_points, learn_rate=lr, 
                                                                   num_step=num_step, kessi=0.0)        
         next_response_gp = triple_fun(next_point_gp)
-
         write_exp_result(out_file_task2_gp, next_response_gp, next_point_gp)
 
         # find best point by STBO
         next_point_stbo, next_point_aux = STBO.find_best_NextPoint_ei(start_points, learn_rate=lr,
                                                                       num_step=num_step, kessi=0.0)
         next_response_stbo = triple_fun(next_point_stbo)
-
         write_exp_result(out_file_task2_stbo, next_response_stbo, next_point_stbo)
+
